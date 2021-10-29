5a5f842dacbf9c8d4b864a424ef4cb915c7c6e73
Muzammil Haider <muzammil.haider@cooperativecomputing.co>
2021-10-29 12:35:31 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// chapter 1
+// ques 1
+// alert("Hello World");
+
+// ques 2
+// alert("Error! Please enter a valid password.");
+
+// ques 3
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+// ques 4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs.");
+
+// ques 5
+// alert("Hello... I can run JS through my web browser's console");
+
+
+
+
+// chapter 2
+// ques 1
+// var username;
+
+// ques 2
+// var myName = "Muzammil Haider";
+
+// ques 3
+// var message = "Hello World";
+// alert(message);
+
+// ques 4
+// var name = "Jhone Doe";
+// var age = "15 years old";
+// var course = "Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(course);
+
+// ques 5
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+// ques 6
