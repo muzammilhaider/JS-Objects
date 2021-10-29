67d97d20d67f7e8ccd0edfad0ebffe1c26bd0ed6
Muzammil Haider <muzammil.haider@cooperativecomputing.co>
2021-10-29 14:06:58 +0500

    chapter 4 completed

--- app.js
+++ app.js
@@ -51,6 +51,74 @@
 // ques 8
 // document.write("Yah! I can write HTML content through Javascript");
 
+// ques 9
 // var data = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 // alert(data);
 
+// chapter 3
+// ques 1
+// var age = "15";
+// alert("I am " + age + " years old");
+
+// ques 2
+// var visitor = 14;
+// alert("You have visited this site " + visitor + " times");
+
+// ques 3
+// var birthYear = 1998;
+// document.write("My birht year is " + birthYear + "<br><br>");
+// document.write("Data type of my declared variable is number");
+
+// ques 4
+// var visitorName = "John Doe";
+// var productTitle = "T-Shirt";
+// var quantity = 5;
+// document.write("<b>"+visitorName + "</b> ordered <b>" + quantity + " " +productTitle + "</b>" + "(s) " + " on XYZ Cloting Store");
+
+
+
+// chapter 4
+// ques 1
+// var name, email, age;
+
+// ques 2
+// legal variables
+// var myName, _myName, $myName, my_name, myname1;
+//illegal variables
+// var @email, 2email, my-email, var, my+email;
+
+// ques 3
+// a)
+// b) numbers, letter, $ and _
+// c) letter, $ and _
+// d) keywords
+
+// chapter 5
+// ques 1
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 + num2;
+// document.write("Sum of " + num1 + " and " + num2 + " is " + result);
+
+// ques 2
+// var num1 = 5;
+// var num2 = 3;
+// var result = num1 - num2;
+// document.write("Subtraction of " + num1 + " and " + num2 + " is " + result);
+
+
+// var num1 = 3;
+// var num2 = 5;
+// var result = num1 * num2;
+// document.write("Multiplication of " + num1 + " and " + num2 + " is " + result);
+
+
+// var num1 = 10;
+// var num2 = 5;
+// var result = num1 + num2;
+// document.write("Division of " + num1 + " and " + num2 + " is " + result);
+
+// var num1 = 10;
+// var num2 = 5;
+// var result = num1 % num2;
+// document.write("Modulus of " + num1 + " and " + num2 + " is " + result);
