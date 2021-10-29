3279fda3cc03e1d27d541c8305b31689de56defa
Muzammil Haider <muzammil.haider@cooperativecomputing.co>
2021-10-29 21:16:11 +0500

    Chapter 6-9 completed

--- app.js
+++ app.js
@@ -122,3 +122,101 @@
 // var num2 = 5;
 // var result = num1 % num2;
 // document.write("Modulus of " + num1 + " and " + num2 + " is " + result);
+
+
+// chapter 6-9
+// ques 1
+// var a = 10;
+// var result;
+// document.write("Result:<br><br>");
+// document.write("The value of a is: " + a);
+// document.write("<br><br>....................<br><br>");
+// ++a;
+// document.write("The value of ++a is: " + a);
+// document.write("<br><br>Now the value of a is: " + a);
+// result = a++;
+// document.write("<br><br>The value of a++ is: " + result);
+// document.write("<br><br>Now the value of a is: " + a);
+// --a;
+// document.write("<br><br>The value of --a is: " + a);
+// document.write("<br><br>Now the value of a is: " + a);
+// result = a--;
+// document.write("<br><br>The value of a-- is: " + result);
+// document.write("<br><br>Now the value of a is: " + a);
+
+// ques 2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+//              1 - 0 + 1 + 1
+// document.write(result);
+// document.write("a is: " + a);
+// document.write("b is: " + b);
+
+// ques 3
+// var name = prompt("Enter name");
+// alert("Hello " + name);
+
+// ques 5
+// var num = +prompt("Enter number");
+// var i;
+// var result;
+// if(num == "" || num == null){
+//     num = 5;
+// }
+// for(i = 1; i <= 10; i++){
+//     result = num * i;
+//     document.write(num + " * " + i + " = " + result + "<br>");
+// }
+
+// ques 6
+// var subj1 = prompt("Enter 1st Subject name");
+// var subj2 = prompt("Enter 2nd Subject name");
+// var subj3 = prompt("Enter 3rd Subject name");
+// var subTotal1 = 100;
+// var subTotal2 = 100;
+// var subTotal3 = 100;
+// var subTotal = subTotal1 + subTotal2 + subTotal3;
+// var obtMarks1 = +prompt("Enter 1st Subject marks");
+// var obtMarks2 = +prompt("Enter 2nd Subject marks");
+// var obtMarks3 = +prompt("Enter 3rd Subject marks");
+// var totalMarks = obtMarks1 + obtMarks2 + obtMarks3;
+// var per1 = (obtMarks1 / subTotal1) * 100;
+// var per2 = (obtMarks2 / subTotal2) * 100;
+// var per3 = (obtMarks3 / subTotal3) * 100;
+// var overAllPercentage = (totalMarks / subTotal) * 100;
+
+// document.write("<table>");
+// document.write("<thead>");
+// document.write("<tr>");
+// document.write("<td><b>Subject</b></td>");
+// document.write("<td><b>Total Marks</b></td>");
+// document.write("<td><b>Obtained Marks</b></td>");
+// document.write("<td><b>Percentage</b></td>");
+// document.write("</tr>");
+// document.write("</thead>");
+// document.write("<tr>");
+// document.write("<td>" + subj1 + "</td>");
+// document.write("<td>" + subTotal1 + "</td>");
+// document.write("<td>" + obtMarks1 + "</td>");
+// document.write("<td>" + per1 + "%</td>");
+// document.write("</tr>");
+// document.write("<tr>");
+// document.write("<td>" + subj2 + "</td>");
+// document.write("<td>" + subTotal2 + "</td>");
+// document.write("<td>" + obtMarks2 + "</td>");
+// document.write("<td>" + per2 + "%</td>");
+// document.write("</tr>");
+// document.write("<tr>");
+// document.write("<td>" + subj3 + "</td>");
+// document.write("<td>" + subTotal3 + "</td>");
+// document.write("<td>" + obtMarks3 + "</td>");
+// document.write("<td>" + per3 + "%</td>");
+// document.write("</tr>");
+// document.write("<tr>");
+// document.write("<td></td>");
+// document.write("<td><b>" + subTotal + "</b></td>");
+// document.write("<td><b>" + totalMarks + "</b></td>");
+// document.write("<td><b>" + overAllPercentage + "%</b></td>");
+// document.write("</tr>");
+// document.write("</table>");
+
